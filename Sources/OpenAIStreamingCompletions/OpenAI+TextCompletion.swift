222057fde6d0eb519c335a04f9b61eba08c5b62b
Michel Storms <stormychel@me.com>
2023-04-25 17:56:38 +0200

    - added frequencyPenalty - added topP

--- Sources/OpenAIStreamingCompletions/OpenAI+TextCompletion.swift
+++ Sources/OpenAIStreamingCompletions/OpenAI+TextCompletion.swift
@@ -9,18 +9,22 @@ extension OpenAIAPI {
         var temperature: Double = 0.2
         
         var presencePenalty: Double = 0.0 // MARK: WIP
+        var frequencyPenalty: Double = 0.0 // MARK: WIP
+        var topP: Double = 1.0 // MARK: WIP
         
         var stream = false
         var stop: [String]?
 
-        public init(prompt: String, model: String = "text-davinci-003", max_tokens: Int = 1500, temperature: Double = 0.2, presencePenalty: Double = 0.0, stop: [String]? = nil) {
+        public init(prompt: String, model: String = "text-davinci-003", max_tokens: Int = 1500, temperature: Double = 0.2, presencePenalty: Double = 0.0, frequencyPenalty: Double = 0.0, topP: Double = 1.0, stop: [String]? = nil) {
             self.prompt = prompt
             self.model = model
             self.max_tokens = max_tokens
             self.temperature = temperature
             
             self.presencePenalty = presencePenalty // MARK: WIP
-
+            self.frequencyPenalty = frequencyPenalty // MARK: WIP
+            self.topP = topP // MARK: WIP
+            
             self.stop = stop
         }
     }
