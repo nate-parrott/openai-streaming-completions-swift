410045b3aaef93115b745db1b17ef2eb322ff924
Stv.X <stv.x@icloud.com>
2023-05-15 09:36:36 +0800

    feat: Add custom domain functionality

--- Sources/OpenAIStreamingCompletions/OpenAI.swift
+++ Sources/OpenAIStreamingCompletions/OpenAI.swift
@@ -2,10 +2,14 @@ import Foundation
 
 public struct OpenAIAPI {
     var apiKey: String
+    var host: String = "api.openai.com"
     var orgId: String?
 
-    public init(apiKey: String, orgId: String? = nil) {
+    public init(apiKey: String, host: String? = nil, orgId: String? = nil) {
         self.apiKey = apiKey
+        if let host = host {
+            self.host = host
+        }
         self.orgId = orgId
     }
 }
